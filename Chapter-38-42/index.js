2f79af1f2c586b5488cfcf6edfb658ec00eb9218
adeenazara2002 <adinazara2001@gmail.com>
2024-11-18 11:11:52 +0500

    Chapter 38-42

--- Chapter-38-42/index.js
+++ Chapter-38-42/index.js
@@ -1,17 +1,34 @@
 // FUNCTIONS, SWITCH STATEMENTS, WHILE... DO-WHILE LOOPS
 // 1. Write a custom function power ( a, b ), to calculate the value of
 // a raised to b.
-function power(a, b) {
-    if (b === 0)
-        return 1;
-    if (b < 0)
-        return 1 / power(a, -b);
-    var result = 1;
-    for (var i = 0; i < b; i++) {
-        result *= a;
+// function power(a: number, b: number): number {
+//   if (b === 0) return 1;
+//   if (b < 0) return 1 / power(a, -b);
+//   let result = 1;
+//   for (let i = 0; i < b; i++) {
+//     result *= a;
+//   }
+//   return result;
+// }
+// console.log(power(2, 3));
+// console.log(power(5, 0));
+// console.log(power(2, -2));
+// 2. Any year is entered through the keyboard. Write a function to
+// determine whether the year is a leap year or not.
+// Leap years ..., 2012, 2016, 2020, ...
+// A year is a leap year if:
+// - It is divisible by 4
+// - It is not divisible by 100, unless it is also divisible by 400
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
     }
-    return result;
+    return false;
 }
-console.log(power(2, 3));
-console.log(power(5, 0));
-console.log(power(2, -2));
+var year = 2021;
+if (isLeapYear(year)) {
+    console.log("".concat(year, " is a leap year"));
+}
+else {
+    console.log("".concat(year, " is not a leap year"));
+}
--- Chapter-38-42/index.ts
+++ Chapter-38-42/index.ts
@@ -3,17 +3,38 @@
 // 1. Write a custom function power ( a, b ), to calculate the value of
 // a raised to b.
 
-function power(a: number, b: number): number {
-     if (b===0) return 1;
-     if (b<0) return 1 / power(a, -b);
-     let result = 1;
-     for(let i =0; i<b; i++){
-        result *= a;
-     }
-     return result;
+// function power(a: number, b: number): number {
+//   if (b === 0) return 1;
+//   if (b < 0) return 1 / power(a, -b);
+//   let result = 1;
+//   for (let i = 0; i < b; i++) {
+//     result *= a;
+//   }
+//   return result;
+// }
+// console.log(power(2, 3));
+// console.log(power(5, 0));
+// console.log(power(2, -2));
+
+// 2. Any year is entered through the keyboard. Write a function to
+// determine whether the year is a leap year or not.
+// Leap years ..., 2012, 2016, 2020, ...
+
+    // A year is a leap year if:
+    // - It is divisible by 4
+    // - It is not divisible by 100, unless it is also divisible by 400
+
+function isLeapYear(year: number):  boolean {
+    if ((year % 4 === 0 && year % 100 !==0) || (year % 400 === 0)) {
+        return true;
+    }
+  return false;
 }
-console.log(power(2,3));
-console.log(power(5,0));
-console.log(power(2,-2));
 
+const year =2021;
+if (isLeapYear(year)) {
+    console.log(`${year} is a leap year`);
+}else{
+    console.log(`${year} is not a leap year`);
 
+}
